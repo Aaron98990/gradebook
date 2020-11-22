62d7a830a5647ef72f4a97e50c8a9276323509ed
Aaron Feleke <aaronfeleke@gmail.com>
2020-11-21 22:38:36 -0600

    login errors work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,21 @@ class SessionsController < ApplicationController
   def new
     render 'new'
   end
+
+  def create
+    user = User.find_by(id: params[:session][:id])
+    if user
+      log_in user
+      redirect_to classes
+    else
+      flash.now[:danger] = 'Invalid user'
+      render 'new'
+    end
+  end
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-get '/login', to: 'sessions#create'
-delete '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
