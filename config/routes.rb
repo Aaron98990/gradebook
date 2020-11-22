250f203a3f443854f859a94d6f3fe7f815ab606e
Aaron Feleke <aaronfeleke@gmail.com>
2020-11-21 21:14:30 -0600

    login/signup/sessions not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  root 'static_pages#home'
+  resources :courses
+  resources :users
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+get '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
